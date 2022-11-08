565e0c71378e39380449bdfcd1f7420c244caf56
KristineGur <115322220+KristineGur@users.noreply.github.com>
2022-11-08 08:10:56 +0400

    create JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector("fa-bars");
+let mobileMenu = document.querySelector(".header_nav"); 
+
+burger.addEventListener("click", function (
+  mobileMenu.classList.toggle
+
+)); 
